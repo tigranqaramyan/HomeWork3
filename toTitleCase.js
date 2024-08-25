bfee5ba09f3f5808eb716b027790b38c0de874d1
tigranqaramyan <tigranqaramyan606@gmail.com>
2024-08-25 19:01:59 +0400

    toTitleCase

---
+++ toTitleCase.js
@@ -0,0 +1,11 @@
+function toTitleCase(str) {
+    return str
+        .split(' ') 
+        .map(word => 
+            word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+        )
+        .join(' ');
+}
+
+
+console.log(toTitleCase("hello world")); 
